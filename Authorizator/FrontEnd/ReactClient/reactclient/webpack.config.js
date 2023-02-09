f8ae1a43e87c06cc06d1adf818ee7367931e010a
Mateusz <mzarzycki1212@gmail.com>
2023-02-09 14:21:12 +0100

    ADD - dodanie reacta TS  z NPX

---
+++ Authorizator/FrontEnd/ReactClient/reactclient/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: 'svg-url-loader'
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'app-client.js',
+        path: path.resolve(__dirname, '../../../wwwroot/js'),
+    }
+};
