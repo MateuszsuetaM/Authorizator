f53060f85bc9f2687450d28df88cab5006b5d53e
Mateusz <mzarzycki1212@gmail.com>
2023-02-16 12:31:23 +0100

    EDIT - komponenty dodawania notatki

--- Authorizator/FrontEnd/ReactClient/reactclient/src/App.tsx
+++ Authorizator/FrontEnd/ReactClient/reactclient/src/App.tsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link, Navigate, Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import NoteForm from './components/NoteForm';
 import NewNote from './components/NewNote';
 import { Container } from 'react-bootstrap';
+import useLocalStorage from './components/useLocalStorage';
+import { v4 as uuidV4 } from "uuid"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export type Note = {
+    id: string
+} & NoteData
+
+export type RawNote = {
+    id: string
+} & RawNoteData
+export type RawNoteData = {
+    title: string
+    markdown: string
+    tagIds: string[]
+}
+
+export type NoteData = {
+    title: string
+    markdown: string
+    tags: Tag[]
+}
+
+export type Tag = {
+    id: string
+    label: string
+}
 
 function App() {
+    const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+    const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+    const notesWithTags = useMemo(() => {
+        return notes.map(note => {
+            return {...note, tags:tags.filter(tag=>note.tagIds.includes(tag.id))}
+        })
+    }, [notes, tags])
+
+    function onCreateNote({ tags, ...data }: NoteData) {
+        setNotes(prevNotes => {
+            return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }
+            ]
+        })
+    }
+    function onAddTag(tag:Tag) {
+        setTags(prevTags=> [...prevTags, tag])
+    }
     return (
+                <BrowserRouter>
         <Container className="my-4">
             <div className="App">
-                <BrowserRouter>
                     <Routes>
-                        <Route path="/new" element={<NewNote />} />
-                        <Route path="/note" element={<NoteForm />} />
+                        <Route path="/" element={<NewNote onSubmit={onCreateNote} onAddTag={onAddTag} availableTags={tags} />} />
+                        <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={onAddTag} availableTags={tags} />} />
                         <Route path="*" element={<Navigate to="/" />} />
 
                         <Route path="/:id">
@@ -20,9 +65,9 @@ function App() {
                             <Route path="edit" element={<h1>Edit</h1>} />
                         </Route>
                     </Routes>
-                </BrowserRouter>
             </div>
         </Container>
+                </BrowserRouter>
     );
 }
 
--- Authorizator/FrontEnd/ReactClient/reactclient/src/components/NewNote.tsx
+++ Authorizator/FrontEnd/ReactClient/reactclient/src/components/NewNote.tsx
@@ -1,4 +1,22 @@
-﻿export function NewNote() {
-    return <h1>New notatka</h1>
+﻿import { NoteData, RawNote, Tag } from '../App';
+import { NoteForm } from './NoteForm'
+
+type NewNoteProps = {
+    onSubmit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
+}
+
+
+export function NewNote({ onSubmit, onAddTag, availableTags }:NewNoteProps) { 
+    return <div>
+        <h1>New notatka</h1>
+        <NoteForm
+            onSubmit={onSubmit} 
+            onAddTag={onAddTag}
+            availableTags={availableTags}
+        />
+        </div>
+
 }
 export default NewNote;
